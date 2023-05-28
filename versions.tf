e6d6a59562ef409a518455814b4fa65b9054a0c4
Jan Černý <iam@c3rny.ninja>
2023-05-28 21:37:14 +0200

    update version configuration 230528-213714

--- versions.tf
+++ versions.tf
@@ -3,15 +3,17 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.9.0"
+      version = "~> 2.9.0"
     }
     jsonnet = {
       source  = "alxrem/jsonnet"
-      version = "2.2.0"
+      version = "~> 2.2.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "1.14.0"
+      version = "~> 1.14.0"
     }
   }
+
+  required_version = "~> 1.4.0"
 }
